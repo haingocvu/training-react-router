e5563447bdf765e2e82109abd3168f30f10697fc
vnhai <haingocvu@hotmail.com>
2018-06-23 20:04:13 +0700

    custom link

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import About from "./components/About";
 import Home from "./components/Home";
+import Contact from "./components/Contact";
+import "./App.css";
 
 class App extends Component {
     render() {
+        const CustonLink = ({label, to, activeOnlyWhenExact}) => {
+            return (
+                <Route 
+                    path={to} 
+                    exact={activeOnlyWhenExact}
+                    children={({match})=>{
+                        let className = match?"active abc":"";
+                        return (
+                            <li className={className}>
+                                <Link to={to}>
+                                    {label}
+                                </Link>
+                            </li>
+                        )
+                    }}
+                />
+            )
+        }
         return (
             <Router>
                 <div className="App">
-                    <nav className="navbar navbar-inverse">
+                    <nav className="navbar navbar-default">
                         <ul className="nav navbar-nav">
-                            <li className="active">
-                                <a>Home</a>
-                            </li>
-                            <li>
-                                <a>About</a>
-                            </li>
+                            <CustonLink label="Home" to="/" activeOnlyWhenExact={true}/>
+                            <CustonLink label="About" to="/about"/>
+                            <CustonLink label="Contact" to="/contact"/>
                         </ul>
                     </nav>
                     <Route path="/" exact component={Home} />
                     <Route path="/about" component={About} />
+                    <Route path="/contact" component={Contact} />
                 </div>
             </Router>
         );
