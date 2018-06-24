a61ca2c217a146b6389ee67884993e31f8415970
vnhai <haingocvu@hotmail.com>
2018-06-24 09:51:54 +0700

    prompt

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react';
+import { Prompt } from "react-router-dom";
 
 class Contact extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            allowLeave: false
+        }
+    }
+
+    setAllowLeave = allowLeave => this.setState({
+        allowLeave
+    })
+
     render() {
+        let { allowLeave } = this.state;
         return (
-            <div>this is contact page</div>
+
+            <div className="row">
+                <div>this is contact page</div>
+
+                <button type="button" className="btn btn-default" onClick={() => this.setAllowLeave(true)}>Set allowLeave to true</button>
+
+                <button style={{ marginLeft: 4 }} type="button" className="btn btn-default" onClick={() => this.setAllowLeave(false)}>Set allowLeave to false</button>
+
+
+                <Prompt when={!allowLeave} message={location => `are you sure you want to go to ${location.pathname}`} />
+            </div>
+
         );
     }
 }
